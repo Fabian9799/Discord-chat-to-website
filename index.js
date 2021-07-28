24b22fa053dcf5d8c9a0005bcce6d4d39b73bc53
ole-is-live <ole.live.twitch@gmail.com>
2021-07-28 11:51:58 +0200

    Add Emojis
    This commit causes emojis to be displayed, but currently only 1 emoji OR only text. Both or more than 1 emoji is currently not possible.

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
 require('dotenv').config();
 const http = require("http");
-// Express
 const express = require('express')
 const app = express()
-const server = http.createServer(app); //create a server
-// Websocket
+const server = http.createServer(app);
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ server });
 const port = 3000
-// Discord.js
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 // Websocket On connection
-wss.on("connection", function (ws, req) {
-})
+wss.on("connection", function(ws, req) {})
 
 // Express / used Public folder
 app.use('/', express.static('public'))
 
 // Start express server 
-server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+server.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
 
 // Discord
 client.on('ready', () => {
-  console.log('Bot online!');
+    console.log('Bot online!');
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-  if(message.channel.id == process.env.DISCORD_CHANNEL_ID && message.channel.guild.id == process.env.DISCORD_SERVER_ID){
-    let data = {
-      "message": message.content,
-      "username": message.author.username,
-      "avatar": message.author.avatar,
-      "id": message.author.id
+    const args = message.content.split(' ');
+    if (message.channel.id == process.env.DISCORD_CHANNEL_ID && message.channel.guild.id == process.env.DISCORD_SERVER_ID) {
+        let data = {
+            "message": message.content,
+            "username": message.author.username,
+            "avatar": message.author.avatar,
+            "id": message.author.id
+        }
+        console.log(message.content);
+
+        if ((message.content.includes('<a') || message.content.includes('<:')) && message.content.includes('>')) {
+            console.log(message.content);
+
+            const { Util } = require('discord.js');
+            for (const rawEmoji of args) {
+                const parsedEmoji = Util.parseEmoji(rawEmoji)
+                if (parsedEmoji.id) {
+                    const extension = parsedEmoji.animated ? ".gif" : ".png";
+                    const url = `https://cdn.discordapp.com/emojis/${parsedEmoji.id + extension}`
+
+                    data = {
+                        "message": "",
+                        "username": message.author.username,
+                        "avatar": message.author.avatar,
+                        "id": message.author.id,
+                        "emoji": url
+                    }
+
+                }
+            }
+        }
+        console.log(data)
+        wss.clients.forEach(function each(client) {
+            client.send(JSON.stringify(data));
+
+        });
     }
-    wss.clients.forEach(function each(client) {
-      client.send(JSON.stringify(data));
-    });
-  }
 });
 
 // Log our bot in using the token from https://discord.com/developers/applications
